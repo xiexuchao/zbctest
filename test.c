d9f37b1b62585b41290f6a3d252f18cbd54804b2
xiexuchao <xiexuchao@foxmail.com>
2017-01-24 22:35:41 -0600

    test

--- test.c
+++ test.c
@@ -13,8 +13,12 @@ int main()
     
     struct zbc_zone *z,*zones=NULL;
     int i, ret=1;
-    enum zbc_reporting_options ro = ZBC_RO_ALL;
     char path[64]="/dev/sdb";
+    
+    enum zbc_reporting_options ro = ZBC_RO_ALL;
+    unsigned long long lba=0;
+    unsigned int nr_zones;
+    int nz=100;
     /*
     unsigned long long sector = 0, nr_sectors = 0;
         
@@ -33,11 +37,68 @@ int main()
 		return 1;
 	}
 	zbc_get_device_info(dev,&info);
-    printf("disk model %d\n",(&info)->zbd_model);
-//	zbc_print_device_info(&info,stdout);
-	
-	
-	
+
+    zones = (zbc_zone_t *) malloc(sizeof(zbc_zone_t) * nz);
+    if ( ! zones  ) {
+            fprintf(stderr, "No memory\n");
+            ret = 1;
+            goto out;
+            
+    }
+        memset(zones, 0, sizeof(zbc_zone_t) * nz);
+
+        /* Get zone information */
+        ret = zbc_report_zones(dev, lba, ro, zones, &nz);
+    if ( ret != 0  ) {
+            fprintf(stderr, "zbc_list_zones failed %d\n", ret);
+            ret = 1;
+            goto out;
+            
+    }
+
+        printf("%u / %u zone%s:\n", nz, nr_zones, (nz > 1) ? "s" : "");
+    for(i = 0; i < (int)nz; i++) {
+                z = &zones[i];
+        if ( zbc_zone_conventional(z)  ) {
+                        printf("Zone %05d: type 0x%x (%s), cond 0x%x (%s), LBA %llu, %llu sectors, wp N/A\n",
+                                                 i,
+                                                 zbc_zone_type(z),
+                                                 zbc_zone_type_str(zbc_zone_type(z)),
+                                                 zbc_zone_condition(z),
+                                                 zbc_zone_condition_str(zbc_zone_condition(z)),
+                                                 zbc_zone_start_lba(z),
+                                                 zbc_zone_length(z));
+                    
+        } else {
+                        printf("Zone %05d: type 0x%x (%s), cond 0x%x (%s), need_reset %d, non_seq %d, LBA %llu, %llu sectors, wp %llu\n",
+                                                 i,
+                                                 zbc_zone_type(z),
+                                                 zbc_zone_type_str(zbc_zone_type(z)),
+                                                 zbc_zone_condition(z),
+                                                 zbc_zone_condition_str(zbc_zone_condition(z)),
+                                                 zbc_zone_need_reset(z),
+                                                 zbc_zone_non_seq(z),
+                                                 zbc_zone_start_lba(z),
+                                                 zbc_zone_length(z),
+                                                 zbc_zone_wp_lba(z));
+                    
+        }
+            
+    }
+
+    //get the number of zones
+    /*
+    ret=zbc_report_nr_zones(dev, lba, ro, &nr_zones);
+    if(ret !=0)
+    {
+        printf("report number error !\n");
+        exit(-1);
+    }
+    */
+
+out:
+
+    zbc_close(dev);	
 
 	return 1;
 }
