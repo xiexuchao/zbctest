c65950d9715b643ae567f4d34c7a016636ba9112
xiexuchao <xiexuchao@foxmail.com>
2017-01-25 11:18:02 -0600

    fix

--- hasmr.c
+++ hasmr.c
@@ -16,12 +16,15 @@ int main()
 	char path[64]="/dev/sdb";
 	
 	ret=smr_open(path,dev);
-	
 	if(ret != 1)
 	{
 		printf("OPEN Failure..\n");
 		exit(-1);
 	}
+	
+	
+	
+	
 	
 	ret=smr_close(dev);
 	if(ret != 1)
@@ -29,7 +32,6 @@ int main()
 		printf("CLOSE Failure..\n");
 		exit(-1);
 	}
-
 
 	return 1;
 }
@@ -75,6 +77,72 @@ int smr_close(struct zbc_device *dev)
 	}
 	printf("CLOSE Failure..\n");
 	return -1;	
+}
+
+
+int smr_report(struct zbc_device *dev,unsigned int index)
+{
+	int ret;
+	unsigned long long lba=0;
+    enum zbc_reporting_options ro=ZBC_RO_ALL;
+    zbc_zone_t *z, *zones=NULL;
+    unsigned int nr_zones=0;
+    
+    ret=zbc_report_nr_zones(dev,lba,ro,&nr_zones);
+    if(ret != 0)
+    {
+    	printf("Report Zone Number Failure!\n");
+    	return -1;
+    }
+    printf("-+-nr_zones=%d\n",nr_zones);
+    
+    if(index > nr_zones)
+    {
+    	printf("Index Error!\n");
+    	return -1;
+    }
+    
+    zones=(zbc_zone_t *)malloc(sizeof(zbc_zone_t)*nr_zones);
+    if (!zones) 
+    {
+		printf("malloc Error!\n");
+		return -1;
+    }
+    memset(zones, 0, sizeof(zbc_zone_t)*nr_zones);
+    
+    ret=zbc_report_zones(dev,lba,ro,zones,&nr_zones);
+    if(ret!=0)
+    {
+    	printf("Report Zone Failure!\n");
+    	return -1;
+    }
+    
+    z=&zones[index];
+    if ( zbc_zone_conventional(z) ) {
+            printf("Zone %05d: type 0x%x (%s), cond 0x%x (%s), LBA %llu, %llu sectors, wp N/A\n",
+                   i,
+                   zbc_zone_type(z),
+                   zbc_zone_type_str(zbc_zone_type(z)),
+                   zbc_zone_condition(z),
+                   zbc_zone_condition_str(zbc_zone_condition(z)),
+                   zbc_zone_start_lba(z),
+                   zbc_zone_length(z));
+        } else {
+            printf("Zone %05d: type 0x%x (%s), cond 0x%x (%s), need_reset %d, non_seq %d, LBA %llu, %llu sectors, wp %llu\n",
+                   i,
+                   zbc_zone_type(z),
+                   zbc_zone_type_str(zbc_zone_type(z)),
+                   zbc_zone_condition(z),
+                   zbc_zone_condition_str(zbc_zone_condition(z)),
+                   zbc_zone_need_reset(z),
+                   zbc_zone_non_seq(z),
+                   zbc_zone_start_lba(z),
+                   zbc_zone_length(z),
+                   zbc_zone_wp_lba(z));
+        }
+        
+        return 1;
+
 }
 
 
